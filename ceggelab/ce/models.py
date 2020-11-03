96b0720c6296aa0e7d19d6d8cb828a6720cfda3c
Your Name <you@example.com>
2020-11-04 01:51:42 +0700

    create models

--- ceggelab/ce/models.py
+++ ceggelab/ce/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class StudentRecord(models.Model):
+    student_name_en = models.CharField(max_length=200)
+    student_name_th = models.CharField(max_length=200)
+    student_ID = models.IntegerField(default=0)
+    type = (('M','Master programs'),
+        ('D','Doctoral programs'),
+        ('P','Postdoctorate'), 
+        ('R','Researcher'))
+
+    student_level = models.CharField(max_length=30, choices= type)
