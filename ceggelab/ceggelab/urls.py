147db9300f366c457730fdb0f7977d8aba85f82d
Your Name <you@example.com>
2020-11-03 16:01:20 +0700

    add urlapp link to view

--- ceggelab/ceggelab/urls.py
+++ ceggelab/ceggelab/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    #localhost:8000/admin
     path('admin/', admin.site.urls),
+    #localhost:8000/
+    path('',include('ce.urls')),
 ]
