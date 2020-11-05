3942fee8b2b83bb2fdb391b7f77d6db1a731911c
Your Name <you@example.com>
2020-11-05 14:36:05 +0700

    berfore push heroku again 5/11/2020 14.35

--- ceggelab/ceggelab/settings.py
+++ ceggelab/ceggelab/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'qjh+&v!@=onn1y_(_a8nhvi@!=9hs$=t2xteqcy)=lr-knmt(5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -119,7 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 LOGIN_URL ='login'
 LOGOUT_REDIRECT_URL = 'login'
